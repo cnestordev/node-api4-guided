fd3a13628a82303c7e6f70754756d1d031e6a9f7
Nestor <ncervan87@gmail.com>
2020-09-03 10:11:35 -0700

    add welcome message

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const server = require("./api/server.js");
 
 const port = process.env.PORT || 5000;
 
+server.get('/', (req, res) => {
+  res.send('<h1>Welcome</h1>')
+})
+
 server.listen(port, () => {
   console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
 });
